1f83a5ce2b3c5d182bae61dc559cc92a802ee543
Jackson Quinn <127517426+j-quinn01@users.noreply.github.com>
2024-05-08 16:55:50 -0400

    committing

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //Hook
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+
+  return (
+    <>
+      <h1>list</h1>
+      {items.length === 0 && <p>no item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
